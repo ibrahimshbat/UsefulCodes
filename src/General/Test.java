2e4fbccb01aefd902f7cc42010d07fc0968a9ea9
a6915654 <i.s.el-sanosi@newcastle.as.uk>
2017-07-11 23:34:12 +0100

    Test collections

---
+++ src/General/Test.java
@@ -0,0 +1,9 @@
+package General;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(ConstantPara.findTheta(50, 3, 100));
+	}
+}
