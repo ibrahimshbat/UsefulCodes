8a543bc3ed0bf5540fbcb16f47475f997069fdc6
Ibrahim <ibrahimsabat@yahoo.com>
2018-10-08 21:56:04 +0100

    Add New Package/Class Statistic

---
+++ src/statistic/UseStatistic.java
@@ -0,0 +1,13 @@
+package statistic;
+
+public class UseStatistic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(Statistic.findCIN(10));
+		System.out.println(Statistic.findCIN(50));
+		System.out.println(Statistic.findCIN(100));
+
+	}
+
+}
