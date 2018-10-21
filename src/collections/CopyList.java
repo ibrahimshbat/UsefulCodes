d6de63d3aaf79dadb8ce686f257d1a195c304cdc
Ibrahim <ibrahimsabat@yahoo.com>
2018-10-21 22:19:31 +0100

    Add Test Class to Show How to Copy an Array List

--- src/collections/CopyList.java
+++ src/collections/CopyList.java
@@ -2,21 +2,12 @@ package collections;
 
 import java.util.ArrayList;
 import java.util.List;
-
+/*
+ * Class to show an example of copying list to a new list
+ */
 public class CopyList {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-//		List<String> names1 = new ArrayList<String>();
-//		names1.add("AAA1");
-//		names1.add("AAA2");
-//		names1.add("AAA3");
-//		System.out.println(names1);
-//		List<String> names2 = new ArrayList<String>(names1);
-//		names2.add("AAA4");
-//		System.out.println("names1="+names1);
-//		System.out.println("names2="+names2);
-		
 		List<String> names1 = new ArrayList<String>();
 		names1.add("AAA1");
 		names1.add("AAA2");
@@ -28,18 +19,7 @@ public class CopyList {
 		names1.add("AAA8");
 		names1.add("AAA9");
 		names1.add("AAA10");
-//		names1.add("AAA11");
-//		names1.add("AAA12");
-//		names1.add("AAA13");
-//		names1.add("AAA14");
-//		names1.add("AAA15");
-//		names1.add("AAA16");
-//		names1.add("AAA17");
-//		names1.add("AAA18");
-//		names1.add("AAA19");
-//		names1.add("AAA20");
 
-		//List<String> ss = names1.subList(0, 7);
 		System.out.println("subList="+names1.size());
 		int size = names1.size()/4;
 		int index =0;
@@ -50,14 +30,6 @@ public class CopyList {
 		System.out.println("List3="+names1.subList(index, (index + size)));
 		index += (size);
 		System.out.println("List4="+names1.subList(index, (index + size)));
-//		double v=9.4;
-//		String sval = String.valueOf(v);
-//		System.out.println("subList="+sval);
-		
-		
-
-		
-		
 
 	}
 
