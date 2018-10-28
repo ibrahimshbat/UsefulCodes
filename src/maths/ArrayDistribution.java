91154f5aee08d73430e17b88de9f761fc13499c1
Ibrahim <ibrahimsabat@yahoo.com>
2018-10-28 22:27:37 +0000

    Class to Find the Distribution in Collection

--- src/maths/ArrayDistribution.java
+++ src/maths/ArrayDistribution.java
@@ -5,19 +5,22 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 import java.util.List;
-
 import com.google.common.base.Function;
 import com.google.common.collect.*;
+
+/*
+ * Class to find Distribution contains of the elements in Coolection
+ */
 public class ArrayDistribution {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		//generate a sorted list of 500 items between 1..100
 		final Random rand = new Random();
 		final List<Integer> list = new ArrayList<Integer>();
 		for (int i = 0; i < 500; i++) {
-		    list.add(rand.nextInt(500) + 1);
+			list.add(rand.nextInt(500) + 1);
 		}
 		Collections.sort(list);
 
@@ -28,20 +31,20 @@ public class ArrayDistribution {
 
 		//create a multimap
 		ListMultimap<Integer, Integer> map = Multimaps.index(list, new Function<Integer, Integer>() {
-		    public Integer apply(Integer i) {
-		        //work out which group the value belongs in
-		        return (i / groupRange) + (i % groupRange == 0 ? 0 : 1);
-		    }
+			public Integer apply(Integer i) {
+				//work out which group the value belongs in
+				return (i / groupRange) + (i % groupRange == 0 ? 0 : 1);
+			}
 		});
 
 		/////////////////////////
 
 		//print it out for diagnosis
 		for (Integer key : map.keySet()) {
-		    List<Integer> value = map.get(key);
-		    System.out.println("Distribution contains " + value.size() + " items from " + value.get(0) + " to " + value.get(value.size() - 1));
+			List<Integer> value = map.get(key);
+			System.out.println("Distribution contains " + value.size() + " items from " + value.get(0) + " to " + value.get(value.size() - 1));
 		}
-		
+
 
 	}
 
