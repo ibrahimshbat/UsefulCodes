5470decd7125b63d3c2d11f267d06e1cc73f807f
ibrahimshbat <ibrahimsabat@yahoo.com>
2018-11-02 00:18:24 +0000

    Delelte Unecassay classes

--- src/maths/TestRandom.java
+++ src/maths/TestRandom.java
@@ -5,20 +5,15 @@ import java.util.Random;
 public class TestRandom {
 
 	public static void main(String[] args) {
-		
-	 	 final double  PROP = 0.634;//0.5;//0.6339745962155613;//1.0;//
-	 	 double randomValue;
-	 	 Random random = new Random();
-	 	 boolean sendAck = false;
-	 	
-	 	
-	 	
-			
-	    	
-			randomValue=random.nextDouble();
-			System.out.println(randomValue);
-			randomValue=random.nextDouble();
-			System.out.println(randomValue);
+
+		final double  PROP = 0.634;//0.5;//0.6339745962155613;//1.0;//
+		double randomValue;
+		Random random = new Random();
+		boolean sendAck = false;
+		randomValue=random.nextDouble();
+		System.out.println(randomValue);
+		randomValue=random.nextDouble();
+		System.out.println(randomValue);
 
 
 	}
