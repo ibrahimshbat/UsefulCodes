eec35d6ec3322f882524785a45b149ad0ea4cea5
ibrahimshbat <ibrahimsabat@yahoo.com>
2018-11-03 22:59:06 +0000

    Add Class to Demonstrate else if statement

--- src/maths/TestIf.java
+++ src/maths/TestIf.java
@@ -1,9 +1,11 @@
 package maths;
 
+/*
+ * Class to use else if statement
+ */
 public class TestIf {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		int i = 2, b = 6, c = 12;
 		if(i>1)
 			System.out.println("i>1");
@@ -11,7 +13,6 @@ public class TestIf {
 			System.out.println("b>5");
 		else
 			System.out.println("c>12");
-
 
 	}
 
