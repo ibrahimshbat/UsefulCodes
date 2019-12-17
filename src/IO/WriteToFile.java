ba65f4997813525f726c13543e23fbdf48e0f147
Ibrahim Elsanosi <ibrahimsabat@yahoo.com>
2019-12-17 13:52:47 +0000

    Add IO Package

---
+++ src/IO/WriteToFile.java
@@ -0,0 +1,36 @@
+package IO;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Map;
+
+import thinkific.user.User;
+
+public class WriteToFile {
+
+	public static void main(String[] args) {
+		PrintWriter referralsData = null;
+		try {
+			// System.out.println("Who is this:"+users.get(0));
+			referralsData = new PrintWriter(new BufferedWriter(
+					new FileWriter(System.getProperty("user.home") + "/Downloads/emailsThinkific.csv", false)));
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		String email = null;
+		ArrayList<User> sortedUsers;
+		for (Map.Entry<String, ArrayList<User>> entry : users.entrySet()) {
+			email = entry.getKey();
+			
+			referralsData.println(email);
+
+		}
+		referralsData.close();	
+
+	}
+
+}
---
+++ src/TestOutOfMemory.java
@@ -0,0 +1,16 @@
+
+public class TestOutOfMemory {
+
+	public TestOutOfMemory() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int [] val = {2,4,6};
+		System.out.println(val[2]);
+
+	}
+
+}
