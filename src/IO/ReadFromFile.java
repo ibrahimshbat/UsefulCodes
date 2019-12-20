b8334cddb78d6d6bfb0ec01e6a07b20090177335
Ibrahim Elsanosi <ibrahimsabat@yahoo.com>
2019-12-20 11:55:15 +0000

    Add ReadToFile Class using FileReader

--- src/IO/ReadFromFile.java
+++ src/IO/ReadFromFile.java
@@ -1,9 +1,29 @@
 package IO;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+
 public class ReadFromFile {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
+		String fullPath = System.getProperty("user.home")+"path";
+
+		BufferedReader reader = new BufferedReader(new FileReader(fullPath));		
+		
+		String aff = reader.readLine();
+		while (aff != null) {
+			//parts = aff.split(" ");
+			//affiliate = new Affiliate((long) Long.parseLong(parts[0]), parts[1], Double.parseDouble(parts[2]), Double.parseDouble(parts[3]));
+		//	affiliates.put(Long.parseLong(parts[0]), affiliate);
+			// read next line
+			//aff = reader.readLine();
+		}
+		//reader.close();
 
 	}
 
